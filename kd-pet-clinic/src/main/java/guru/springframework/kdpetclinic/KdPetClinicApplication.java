e72efe05a17d9be57d3026f979731c71a09070fb
kdeepshikha <kdeepshikha@vmware.com>
2021-03-25 15:16:19 -0400

    Initial project add

---
+++ kd-pet-clinic/src/main/java/guru/springframework/kdpetclinic/KdPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.kdpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdPetClinicApplication.class, args);
+	}
+
+}
---
+++ kd-pet-clinic/src/test/java/guru/springframework/kdpetclinic/KdPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.kdpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
